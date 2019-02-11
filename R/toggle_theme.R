f1960aa2b5480f07f11ad2f06e56e35d3ec9ebf8
Mikko Marttila <mikko.marttila@orionpharma.com>
2019-02-11 13:37:08 +0200

    Take arguments in theme_toggle()

--- R/toggle_theme.R
+++ R/toggle_theme.R
@@ -1,14 +1,18 @@
 
-theme_toggler <- function(themes) {
-  n <- length(themes)
+theme_toggler <- function() {
   i <- 0
-  function() {
+  function(themes = getOption("themetoggle.themes")) {
+    n <- length(themes)
     i <<- (i + 1) %% n
     theme <- themes[1 + i]
     rstudioapi::applyTheme(theme)
   }
 }
 
-toggle_theme <- theme_toggler(
-  getOption("themetoggle.themes", default = c("Monarch", "Crimson Editor"))
-)
+#' Toggle RStudio themes
+#'
+#' Applies an RStudio them on invocation, cycling through given themes.
+#'
+#' @param themes a vector of theme names to toggle through
+#' @export
+toggle_theme <- theme_toggler()
