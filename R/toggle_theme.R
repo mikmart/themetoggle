bf3e63c7c511c858307138e84251294508df826b
Mikko Marttila <mikmart@mikkomarttila.com>
2019-02-11 17:45:54 +0200

    Rework theme toggling
    A closure-based counter has the disadvantage that it can't pick up
    from the middle of a theme vector: so in the beginning of a new
    session, if your currently applied theme was the second theme in
    the theme vector, the first invocation of toggle_theme() wouldn't
    do anything.

--- R/toggle_theme.R
+++ R/toggle_theme.R
@@ -1,17 +1,25 @@
-theme_toggler <- function() {
-  i <- 0
-  function(themes = getOption("themetoggle.themes")) {
-    n <- length(themes)
-    i <<- (i + 1) %% n
-    theme <- themes[1 + i]
-    rstudioapi::applyTheme(theme)
-  }
-}
-
 #' Toggle RStudio themes
 #'
 #' Applies an RStudio them on invocation, cycling through given themes.
 #'
 #' @param themes a vector of theme names to toggle through
 #' @export
-toggle_theme <- theme_toggler()
+toggle_theme <- function(themes = getOption("themetoggle.themes")) {
+  rstudioapi::applyTheme(next_theme(themes))
+}
+
+next_theme <- function(themes = getOption("themetoggle.themes")) {
+  themes[next_index(themes)]
+}
+
+next_index <- function(themes = getOption("themetoggle.themes")) {
+  1L + current_index(themes) %% length(themes)
+}
+
+current_index <- function(themes = getOption("themetoggle.themes")) {
+  match(current_theme(), themes, nomatch = 0L)
+}
+
+current_theme <- function() {
+  rstudioapi::getThemeInfo()$editor
+}
